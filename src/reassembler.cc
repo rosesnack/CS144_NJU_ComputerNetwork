6d552aeeb0215d5f3234c15d2462836c7c0e36e0
Chen Dedan <221220159@smail.nju.edu.cn>
2024-10-22 15:25:41 +0800

    1

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -5,34 +5,35 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
   // Your code here.
   uint64_t last_index = first_index + data.length() - 1;
   if (data.empty() || last_index < begin_i_() || first_index > end_i_()) {
-    if (is_last_substring) 
-      output_.writer().close();
+    if (is_last_substring) output_.writer().close();
     return;
   }
+
   if (is_last_substring && last_index <= end_i_())
     has_end = true;
+
   if (first_index <= begin_i_()) {
     data = data.substr(begin_i_() - first_index);
     output_.writer().push(data);
   }
   else if (first_index > begin_i_()) {
-    if (last_index > end_i_()) {
+    if (last_index > end_i_()) 
       data = data.substr(0, end_i_() - first_index + 1);
-    }
     mergeString(first_index, data);
-    return;
   }
-  if (is_last_substring) {
-    output_.writer().close();
-  }
+  
   auto x = myMap.begin();
   while (x != myMap.end() && x->first <= begin_i_()) {
     uint64_t i = x->first;
     string d = x->second;
     x = myMap.erase(x);
-    bool flag = false;
-    if (has_end && myMap.empty()) flag = true;
-    insert(i, d, flag);
+    if (i + d.length() - 1 < begin_i_())
+      continue;
+    d = d.substr(begin_i_() - i);
+    output_.writer().push(d);
+  }
+  if (has_end && myMap.empty()) {
+    output_.writer().close();
   }
 }
 
@@ -49,10 +50,8 @@ uint64_t Reassembler::bytes_pending() const
 void Reassembler::mergeString(uint64_t first_index, string data) {
     auto it = myMap.lower_bound(first_index);
     auto next = it;
-    if (it != myMap.begin() && (it == myMap.end() || it->first > first_index)) {
+    if (it != myMap.begin() && (it == myMap.end() || it->first > first_index))
         it--;
-        next = std::next(it);
-    }
     int newStart = first_index;
     string newStr = data;
 
